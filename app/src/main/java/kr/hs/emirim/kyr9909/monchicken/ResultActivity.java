c53e1aa79c4987815385888a15c65920a33e77cc
Yurim Kang <kyr9909@naver.com>
2016-03-28 20:14:04 +0900

    #2 add Result Activity

---
+++ app/src/main/java/kr/hs/emirim/kyr9909/monchicken/ResultActivity.java
@@ -0,0 +1,17 @@
+package kr.hs.emirim.kyr9909.monchicken;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by appcreator16 on 2016. 3. 28..
+ */
+public class ResultActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
