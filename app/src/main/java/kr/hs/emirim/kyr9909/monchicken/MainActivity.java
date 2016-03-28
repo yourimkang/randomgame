369fca2d48f025567dd23e9d4d9b9a3eebcf9263
Yurim Kang <kyr9909@naver.com>
2016-03-28 20:41:08 +0900

    #7 implement input user name

--- app/src/main/java/kr/hs/emirim/kyr9909/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/monchicken/MainActivity.java
@@ -2,12 +2,23 @@ package kr.hs.emirim.kyr9909.monchicken;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+
+    EditText mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mName = (EditText) findViewById(R.id.name);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mName.setText("");
     }
 }
