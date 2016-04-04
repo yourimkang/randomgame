9d23a45c974c0267f615d46b2c5a2f5e3e027fdc
Yurim Kang <kyr9909@naver.com>
2016-04-04 20:13:10 +0900

    #7 add image resouces

--- app/src/main/java/kr/hs/emirim/kyr9909/monchicken/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/monchicken/MainActivity.java
@@ -1,36 +1,41 @@
 package kr.hs.emirim.kyr9909.monchicken;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+        import kr.hs.emirim.kyr9909.monchicken.R;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     EditText mName;
     Button mButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//객체 이어주는 것, 한번만 처리하는것
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mName = (EditText) findViewById(R.id.name);
+        mName = (EditText)findViewById(R.id.name);//id값을 가지고 객체를 찾아서 가져와줌,activity_main에서 찾아서 가져와줌/ mName이 EditText이므로 그에 맞게 타입캐스팅해준다. xml에 있는 EditText를 맘대로 가져올 수 있다.
         mButton = (Button) findViewById(R.id.btn_show_me_the_chicken);
         mButton.setOnClickListener(this);
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume() { //매번 처리해야 되는 것, 화면이 보여질 때마다
         super.onResume();
-        mName.setText(" ");
+        mName.setText("");//text공간을 비우게 해줌
     }
 
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "배고파요!", Toast.LENGTH_LONG).show();
-
+        Toast.makeText(this,"배고파워!", Toast.LENGTH_LONG).show();//현재 문맥을 넘겨줌 , duration :기간
     }
 }
